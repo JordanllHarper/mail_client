40dab74473e34559fdae10dfea78802972acf5da
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-08-28 09:19:43 +0100

    Implement header parsing

--- src/email/util/header_parsing.rs
+++ src/email/util/header_parsing.rs
@@ -4,6 +4,8 @@ use crate::email::{
 };
 use mailparse::*;
 
+//Parses the content type and associates that with the represented enum
+//NOTE: This may expand in the future
 pub fn parse_content_type(content_type_string: String) -> Option<ContentType> {
     match content_type_string.as_str() {
         "text/plain" => Some(ContentType::TextPlain),
@@ -12,13 +14,14 @@ pub fn parse_content_type(content_type_string: String) -> Option<ContentType> {
     }
 }
 
+//Parses the data out of the header
+//NOTE: This may expand in the future
 pub fn parse_headers(m: &[u8]) -> eyre::Result<crate::email::email_message::Headers> {
     let result = mailparse::parse_mail(m)?;
 
     let from = (&result.headers)
         .get_first_value("From")
         .ok_or(crate::email::errors::EmailError::ParseHeaderError)?;
-
     let to = result
         .headers
         .get_first_value("To")
