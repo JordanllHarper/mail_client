5a429d4d0d62bbb622e41c24515f3c1d15865c2b
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-08-28 17:50:34 +0100

    Change email account so items are accessible and change home screen to be styled based on account

--- src/email/email_account.rs
+++ src/email/email_account.rs
@@ -1,9 +1,9 @@
 use super::user_credentials::UserCredentials;
 
 pub struct EmailAccount {
-    account_type: AccountType,
+    pub account_type: AccountType,
     //credentials for this account
-    credentials: UserCredentials,
+    pub credentials: UserCredentials,
 }
 
 //TODO: Add more supported once up and running
--- src/ui/home_screen.rs
+++ src/ui/home_screen.rs
@@ -1,10 +1,14 @@
 use std::rc::Rc;
 
+use ratatui::style::{Color, Stylize};
+use ratatui::text::{Line, Span, Text};
 use ratatui::{layout, Frame};
 
 use ratatui::layout::Direction;
 use ratatui::prelude::{Backend, Constraint, Rect};
-use ratatui::widgets::{Block, Borders};
+use ratatui::widgets::{Block, Borders, List, ListItem};
+
+use crate::email::email_account::EmailAccount;
 
 pub fn home_screen<'a, B: ratatui::backend::Backend>(frame: &'a mut Frame<B>) {
     let layout: Rc<[Rect]> = ratatui::layout::Layout::default()
@@ -19,7 +23,16 @@ pub fn home_screen<'a, B: ratatui::backend::Backend>(frame: &'a mut Frame<B>) {
         )
         .split(frame.size());
 
-    let account_pane = account_list_pane();
+    let test_accounts = vec![EmailAccount {
+        account_type: crate::email::email_account::AccountType::Gmail,
+        credentials: crate::email::user_credentials::UserCredentials {
+            email: "an_email_that_is_toooooooooo_long@somewhere.com".to_owned(),
+            password: "123bad".to_owned(),
+            authentication_identity: "identity".to_owned(),
+            secret: "secret".to_owned(),
+        },
+    }];
+    let account_pane = account_list_pane(test_accounts);
     frame.render_widget(account_pane, layout[0]);
     let email_list_pane = email_list_pane();
     frame.render_widget(email_list_pane, layout[1]);
@@ -28,8 +41,24 @@ pub fn home_screen<'a, B: ratatui::backend::Backend>(frame: &'a mut Frame<B>) {
 }
 
 //account list component
-fn account_list_pane<'a>() -> Block<'a> {
-    Block::default().borders(Borders::ALL).title("Accounts")
+fn account_list_pane<'a>(accounts: Vec<EmailAccount>) -> List<'a> {
+    let list_items: Vec<ListItem> = accounts
+        .iter()
+        .map(|a| {
+            let color: Color = match a.account_type {
+                crate::email::email_account::AccountType::Outlook => Color::Blue,
+                crate::email::email_account::AccountType::Gmail => Color::Red,
+            };
+            let initial = &a.credentials.email;
+
+            let content = Line::from(Span::raw(format!("{initial}")));
+            ListItem::new(content).bg(color)
+        })
+        .into_iter()
+        .collect();
+    let list =
+        List::new(list_items).block(Block::default().borders(Borders::ALL).title("Accounts"));
+    list
 }
 
 //email list component
